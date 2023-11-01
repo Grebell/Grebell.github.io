fd7fc42784cb8808935f0052de37cce20604190d
Grebell <100739501+Grebell@users.noreply.github.com>
2023-11-01 16:11:42 -0400

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -25,7 +25,7 @@
         {
             //Black BG for the canvas
             //translucent BG to show trail
-            ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
+            ctx.fillStyle = "rgba(0, 0, 0, 0)";
             ctx.fillRect(0, 0, c.width, c.height);
 
             ctx.fillStyle = "#f4427d";//green text
